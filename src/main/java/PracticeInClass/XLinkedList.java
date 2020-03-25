4dc2583f7da84b21ae70c093c3196da06a397a38
Eltaj-R <eltacrecebov@mail.ru>
2020-03-25 13:24:43 +0300

    #HomeworkUnfinished

---
+++ src/main/java/PracticeInClass/XLinkedList.java
@@ -0,0 +1,179 @@
+package PracticeInClass;
+
+
+import java.util.StringJoiner;
+
+public class XLinkedList {
+
+    static class Node {
+        final int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    Node head;
+
+    void prepend(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+    }
+
+    void prepend2(int value) {
+        Node node = new Node(value, head);
+        head = node;
+    }
+
+    void prepend3(int value) {
+        head = new Node(value, head);
+    }
+
+    void append(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+        } else {
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = node;
+        }
+    }
+
+    public String represent() {
+        StringJoiner sj = new StringJoiner(",", "LL:(", ")");
+        Node curr = head;
+        while (curr != null) {
+            sj.add(String.valueOf(curr.value));
+            curr = curr.next;
+        }
+        return sj.toString();
+    }
+
+    public String represent2() {
+        StringJoiner sj = new StringJoiner(",", "LL:(", ")");
+        for (Node curr = head; curr != null; curr = curr.next) {
+            sj.add(String.valueOf(curr.value));
+        }
+        return sj.toString();
+    }
+
+    private void attach_next(Node curr, StringJoiner sj) {
+        if (curr == null) return;
+        sj.add(String.valueOf(curr.value));
+        attach_next(curr.next, sj);
+    }
+
+    public String represent3r() {
+        StringJoiner sj = new StringJoiner(",", "LL:(", ")");
+        attach_next(head, sj);
+        return sj.toString();
+    }
+
+    int size() {
+        int count = 0;
+        for (Node curr = head; curr != null; curr = curr.next) {
+            count++;
+        }
+        return count;
+    }
+
+    int sizer() {
+        throw new RuntimeException("Should be implemented at home");
+    }
+
+    int sizetr() {
+        throw new RuntimeException("Should be implemented at home");
+    }
+
+    boolean contains(int element) {
+        return checkerIfContains(element) >= 0;
+
+    }
+
+    int checkerIfContains(int element) {
+        int count = -1;
+
+        for (Node curr = head; curr != null; curr = curr.next) {
+            count++;
+
+
+            if (curr.value == element) {
+                count++;
+                break;
+
+            } else count--;
+        }
+        return count;
+    }
+    //used similar method as in ArrayList
+
+    int indexOf(int element) {
+        if (contains(element)) {
+            int index = -1;
+            for (Node curr = head; curr != null; curr = curr.next) {
+                index++;
+
+                if (curr.value == element) {
+                    break;
+                }
+            }
+            return index;
+        } else {
+            return -1;
+        }
+    }
+
+
+    void containsr(int element) {
+    }
+
+    void reverse() {
+
+        for (Node curr = null; curr != head; ) {
+            curr = curr.next;
+        }
+    }
+
+    void reverser() {
+    }
+
+    void merge(Node head2) {
+
+    }
+
+    boolean containsAt(int index, int element) {
+        throw new IllegalArgumentException("containsAt:Should be implemented t home");
+    }
+
+
+    void deleteHead() {
+
+
+    }
+
+    void deleteTail() {
+    }
+
+    void deleteAt(int index) {
+    }
+
+    void deleteValue(int value) {
+    }
+
+    void insertAfter(int index, int value) {
+    }
+
+    void insertBefore(int index, int value) {
+    }
+
+}
---
+++ src/main/java/PracticeInClass/XLinkedListApp.java
@@ -0,0 +1,24 @@
+package PracticeInClass;
+
+
+public class XLinkedListApp {
+    public static void main(String[] args) {
+        XLinkedList xl = new XLinkedList();
+        xl.prepend(1); // LL: 1
+        xl.prepend(2); // LL: 2,1
+        xl.prepend3(3); // LL: 3,2,1
+        xl.append(10);   // LL: 3,2,1,10
+        xl.append(20);   // LL: 3,2,1,10,20
+        System.out.println(xl.represent());
+        System.out.println(xl.represent2());
+        System.out.println(xl.represent3r());
+
+        System.out.println(xl.indexOf(3));
+        System.out.println(xl.indexOf(2));
+        System.out.println(xl.indexOf(1));
+        System.out.println(xl.indexOf(10));
+        System.out.println(xl.size());
+
+
+    }
+}
